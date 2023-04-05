993dbaed8d5d0da81f953af4d9abe0b5687b8107
ysb777 <yusak.surya.b@gmail.com>
2023-04-05 16:26:51 +0700

    progress Fitur Landing Page ref#10

---
+++ YuBu/resources/views/Landing_Page.blade.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="/css/styling.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+</head>
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+  <section class="nav">
+    <div class="name">
+      <h1>YUKBUDAYAKU</h1>
+    </div>
+    <div class="navbar">
+      <ul>
+        <li><a href="#">Selamat Datang</a></li>
+        <li><a href="/Artikel.html">Artikel Budaya</a></li>
+        <li><a href="#">Destinasi Budaya</a></li>
+        <li><a href="#">about us</a></li>
+        <li><a href="#"><img src="/img/Landingpage/Vector.svg"></a></li>
+      </ul>
+    </div>
+  </section>
+  <section class="hero">
+    <div class="texthero">
+      <h1>Lestarikan Budaya Kita Bersama <span style="color :goldenrod">YuBu</span> </h1>
+      <p>
+          Ayo, mari kita jelajahi dan belajar tentang keundahan budaya dari berbagai penjuru Nusantara bersama
+          kami disini! Temukan pengalaman belajar yang menarik dan memuaskan tentang warisan budaya yang kaya dan mempesona
+      </p>
+    </div>
+  </section>
+  <section class="landing-artikel">
+    <div class="text-artikel">
+      <h1><u>Destinasi Kebudayaan</u></h1>
+      <p>Eksplorasi destinasi kebudayaan yang memukau dan memesoan di seluruh nusantara <a href="#"><span></span style="text-align:end"> view all</a></p>
+    </div>
+    <section class ="content-container">
+      <div class="card">
+          <img src="asset/image/Artikel/Barong.jpg"  alt="card1">
+          <div class="card-body">
+            <center><h1>Theater Barong</h1></center>
+            <p class="card-text">Barong adalah salah satu bentuk teater tradisional yang berasal dari Bali, Indonesia
+                                 Dalam pertunjukan Barong, aktor mengenakan kostum berbentuk kepala singa...</p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="/img/Landingpage/highlight-artikel1.jpeg"  alt="card2">
+          <div class="card-body">
+            <span><h3>Bali</h3></span>
+            <center><h1>Indahnya Alunan Gamelan</h1></center>
+            <p class="card-text">Alunan gamelan begitu indah dan memukau memancarkan kekayaan dan keindahan
+                                 musik tradisional Indonesia...</p>
+            <p class="card-text"><small class="text-muted"></small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="asset/image/Artikel/Tenun.jpeg"  alt="card3">
+          <div class="card-body">
+            <center><h1>Membuat Kain tradisional Baduy</h1></center>
+            <p class="card-text">Proses penenunan kain Baduy biasanya dilakukan oleh perempuan dengan cara yang sama seperti
+                                 yang dilakukan oleh pendahulu mereka selama berabad-abad...</p>
+            <p class="card-text"><small class="text-muted"></small></p>
+          </div>
+        </div>
+      </section>
+  </section>
+  <section class="landing-artikel">
+    <div class="text-artikel">
+      <h1><u>Destinasi Kebudayaan</u></h1>
+      <p>jelajahi warisan budaya Indonesia yang kaya dan bervariasi melalui artikel terbaru yang kami sajikan &nbsp &nbsp <a href="#"><span></span style="text-align:end"> view all</a></p>
+    </div>
+    <div class="highlight-artikel">
+      <div class="card" style="width: 18rem;">
+        <img src="/img/Landingpage/highlight-artikel1.jpeg" class="card-img-top" alt="highlight1" style="width:100%">
+        <div class="card-body">
+          <h5 class="card-title">Theater Barong</h5>
+          <p class="card-text">Barong adalah salah satu bentuk teater tradisional yang berasal dari Bali, Indonesia. Dalam pertunjukan Barong, aktor mengenakan kostum berbentuk kepala singa . . .</p>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="/img/Landingpage/highlight-artikel2.jpeg" class="card-img-top" alt="highlight2" style="width:100%">
+        <div class="card-body">
+          <h5 class="card-title">Indahnya Alunan Gamelang</h5>
+          <p class="card-text">Alunan gamelan begitu indah dan memukau, memancarkan kekayaan dan keindahan musik tradisional Indonesia. . .</p>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+          <img src="/img/Landingpage/highlight-artikel3.jpeg" class="card-img-top" alt="highlight3" style="width:100%">
+          <div class="card-body">
+            <h5 class="card-title">Membuat Kain Tradisional Baduy</h5>
+            <p class="card-text">Proses penenunan kain Baduy biasanya dilakukan oleh perempuan dengan cara yang sama seperti yang dilakukan oleh pendahulu mereka selama berabad-abad. . .</p>
+          </div>
+      </div>
+    </div>
+  </section>
+  <section class="hero">
+    <div class="texthero">
+      <h1>Tentang Kami</h1>
+      <p>
+        Website yang bernama “YuBu: Yuk BudayaKu” ini dikembangkan dengantujuan sebagai  sarana edukasi agar masyarakat dapat melestarikan budaya asli Indonesia. 
+      </p>
+      <p>
+        Website ini memiliki fitur-fitur yang dapat membantu masyarakat dalam mengetahui destinasi wisata budaya di Indonesia, belajar bahasa daerah, belajar kesenian dll.
+      </p>
+    </div>
+    <div class="video-aboutus">
+      <video width="320" height="240" controls>
+        <source src="https://www.youtube.com/watch?v=DOsFf4U5p5w" type="video/mp4">
+      Your browser does not support the video tag.
+      </video>
+    </div>
+  </section>
+  <section class="gallery">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="..." alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="..." alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="..." alt="Third slide">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="FAQ">
+    <div>
+      
+    </div>
+  </section>     
+</body>
+</html>
+      

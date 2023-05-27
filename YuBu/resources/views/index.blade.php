0584571e0ba0fcaeab025451bbb20ad62606664f
Aebil Taskari Rusydi <aebiltaskari@gmail.com>
2023-05-27 20:53:22 +0700

    up

--- YuBu/resources/views/index.blade.php
+++ YuBu/resources/views/index.blade.php
@@ -166,32 +166,6 @@
                   labore reiciendis laboriosam quos at eum, sed sequi tempore
                   perspiciatis magnam iste quas sit minima provident!
                 </p>
-                <h3 class="testimonial-date">April 27, 2023</h3>
-
-                <div class="testimonial-profile" style="justify-content: center;flex-direction: column;row-gap: 1.4rem;">
-                  <img
-                    src="{{ asset('frontend/assets/images/testimonial1.jpg') }}"
-                    alt=""
-                    class="testimonial-profile-img"
-                  />
-
-                  <div class="testimonial-profile-data">
-                    <span class="testimonial-profile-name">John Doe</span>
-                    <span class="testimonial-profile-detail"
-                      >Director of a Company</span
-                    >
-                  </div>
-                </div>
-              </div>
-              <div class="testimonial-card swiper-slide" style="text-align: center;">
-                <div class="testimonial-quote">
-                  <i class="bx bxs-quote-alt-left"></i>
-                </div>
-                <p class="testimonial-description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                  labore reiciendis laboriosam quos at eum, sed sequi tempore
-                  perspiciatis magnam iste quas sit minima provident!
-                </p>
                 <h3 class="testimonial-date">March 27, 2021</h3>
 
                 <div class="testimonial-profile" style="justify-content: center;flex-direction: column;row-gap: 1.4rem;">

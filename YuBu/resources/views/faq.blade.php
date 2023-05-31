a72e2b24bf5c302892e6e55c3c21588ab6b94f74
dewaldi1 <115633440+dewaldi1@users.noreply.github.com>
2023-05-31 15:21:46 +0700

    Update

--- YuBu/resources/views/faq.blade.php
+++ YuBu/resources/views/faq.blade.php
@@ -2,52 +2,51 @@
 
 @section('content')
 <style>
+    .container h1 {
+        color: #fff;
+        text-align: center;
+    }
 
-.container h1{
-  color: #fff;
-  text-align: center;
-}
+    details {
+        background-color: #F4BD4F;
+        color: #fff;
+        font-size: 1.5rem;
+    }
 
-details{
-  background-color: #F4BD4F;
-  color: #fff;
-  font-size: 1.5rem;
-}
+    .expand {
+        background-color: #f6e4c3;
+        color: black;
+        font-size: 12pt;
+    }
 
-.expand {
-    background-color: #f6e4c3;
-    color: black;
-    font-size: 12pt;
-}
+    summary {
+        padding: .5em 1.3rem;
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        transition: height 1s ease;
+    }
 
-summary {
-  padding: .5em 1.3rem;
-  list-style: none;
-  display: flex;
-  justify-content: space-between;  
-  transition: height 1s ease;
-}
+    summary::-webkit-details-marker {
+        display: none;
+    }
 
-summary::-webkit-details-marker {
-  display: none;
-}
+    summary:after {
+        content: "\002B";
+    }
 
-summary:after{
-  content: "\002B";
-}
+    details[open] summary {
+        border-bottom: 1px solid #aaa;
+        margin-bottom: .5em;
+    }
 
-details[open] summary {
-    border-bottom: 1px solid #aaa;
-    margin-bottom: .5em;
-}
+    details[open] summary:after {
+        content: "\00D7";
+    }
 
-details[open] summary:after{
-  content: "\00D7";
-}
-
-details[open] div{
-  padding: .5em 1em;
-}
+    details[open] div {
+        padding: .5em 1em;
+    }
 </style>
 <section class="products section container" id="course">
     <h2 class="section-title">FAQ</h2>
@@ -58,8 +57,9 @@ details[open] div{
         <details>
             <summary>Apa itu YuBu : Yuk BudayaKu?</summary>
             <div class="expand">
-            YuBu: Yuk BudayaKu merupakan website yang mengedukasi masyarakat agar lebih melek terhadap budaya, 
-            meningkatkan kesadaran budaya, melestarikan kebudayaan Indonesia, dan mempermudah akses informasi budaya. 
+                YuBu: Yuk BudayaKu merupakan website yang mengedukasi masyarakat agar lebih melek terhadap budaya,
+                meningkatkan kesadaran budaya, melestarikan kebudayaan Indonesia, dan mempermudah akses informasi
+                budaya.
             </div>
         </details>
     </div>
@@ -68,7 +68,8 @@ details[open] div{
         <details>
             <summary>Apa keuntungan menggunakan Web YuBu : Yuk BudayaKu?</summary>
             <div class="expand">
-            Belajar budaya dengan budayawan langsung, belajar lebih dalam mengenai budaya Indonesia dan memberikan kemudahan akses belajar budaya.
+                Belajar budaya dengan budayawan langsung, belajar lebih dalam mengenai budaya Indonesia dan memberikan
+                kemudahan akses belajar budaya.
             </div>
         </details>
     </div>
@@ -77,8 +78,10 @@ details[open] div{
         <details>
             <summary>Apakah YuBu menyediakan kursus atau pelajaran interaktif ?</summary>
             <div class="expand">
-             Ya, situs ini meyediakan beragam kursus dan pelajaran interaktif yang dapat anda ikuti. Kami menawarkan modul pembelajaran terstruktur, vidio 
-             pembelajaran dan latihan soal. Dengan kursus ini anda dapat memperdalaman anda tentang budaya dan meningkatkan keterampilan anda secara efektif.   
+                Ya, situs ini meyediakan beragam kursus dan pelajaran interaktif yang dapat anda ikuti. Kami menawarkan
+                modul pembelajaran terstruktur, vidio
+                pembelajaran dan latihan soal. Dengan kursus ini anda dapat memperdalaman anda tentang budaya dan
+                meningkatkan keterampilan anda secara efektif.
             </div>
         </details>
     </div>
@@ -87,7 +90,7 @@ details[open] div{
         <details>
             <summary>Apakah ada biaya untuk mengakses kursus YuBu?</summary>
             <div class="expand">
-            Ya, Untuk mengakses konten dan layanan kursus ada biaya terkait.
+                Ya, Untuk mengakses konten dan layanan kursus.
             </div>
         </details>
     </div>

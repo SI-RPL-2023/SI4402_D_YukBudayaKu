48a7f9aee4fdda3a86bd817eddc762bc4a7734c5
vadelaristi <vadelaristi09@gmail.com>
2023-05-31 17:48:04 +0700

    update terbaru

--- YuBu/resources/views/faq.blade.php
+++ YuBu/resources/views/faq.blade.php
@@ -90,7 +90,7 @@
         <details>
             <summary>Apakah ada biaya untuk mengakses kursus YuBu?</summary>
             <div class="expand">
-                Ya, Untuk mengakses konten dan layanan kursus ada biaya terkait.
+                Ya, Untuk mengakses konten dan layanan kursus ada biaya terkait. Untuk biayanya bisa dilihat di 
             </div>
         </details>
     </div>
